fdb28850bc8572d8427ee7224e4c98b34bb4c296
Jonas Cederberg <jonas.cederberg@ferroamp.se>
2024-01-11 16:07:51 +0100

    Added neo-tree git keybindings

--- config/nvim/lua/configs/neo-tree.lua
+++ config/nvim/lua/configs/neo-tree.lua
@@ -37,6 +37,12 @@ function M.config()
         width = 33,
         mappings = {
           ["o"] = "open",
+          ["gu"] = "git_unstage_file",
+          ["ga"] = "git_add_file",
+          ["gr"] = "git_revert_file",
+          ["gc"] = "git_commit",
+          ["gp"] = "git_push",
+          ["gg"] = "git_commit_and_push"
         },
       },
       filesystem = {
