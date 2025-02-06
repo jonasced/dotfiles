122ce86f6013d9d5d30b06bd0f185fe25f8035cc
Jonas Cederberg <jonas.cederberg@gmail.com>
2025-02-06 17:48:40 +0100

    Add jupyter plugins

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -357,6 +357,34 @@ return {
     'Exafunction/codeium.vim',
     event = 'BufEnter'
   },
+  -- jupyter integration
+  {
+    "SUSTech-data/neopyter",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter', -- neopyter don't depend on `nvim-treesitter`, but does depend on treesitter parser of python
+      'AbaoFromCUG/websocket.nvim',  -- for mode='direct'
+    },
+
+    ---@type neopyter.Option
+    opts = {
+        mode="direct",
+        remote_address = "127.0.0.1:9001",
+        file_pattern = { "*.ju.*" },
+        on_attach = function(bufnr)
+            -- do some buffer keymap
+        end,
+    },
+  },
+  {
+      "benlubas/molten-nvim",
+      version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+      build = ":UpdateRemotePlugins",
+      init = function()
+          -- this is an example, not a default. Please see the readme for more configuration options
+          vim.g.molten_output_win_max_height = 12
+      end,
+  },
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
