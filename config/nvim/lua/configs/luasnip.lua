dbbf6b5c4a06334699ca8c95265a5c251270b208
Jonas Cederberg <jonas.cederberg@gmail.com>
2026-03-10 11:06:14 +0100

    Add div snippet

--- config/nvim/lua/configs/luasnip.lua
+++ config/nvim/lua/configs/luasnip.lua
@@ -9,6 +9,30 @@ function M.config()
       delete_check_events = "TextChanged",
       region_check_events = "CursorMoved",
     })
+    -- Setup for snippets
+    local s = luasnip.snippet
+    local i = luasnip.insert_node
+    local f = luasnip.function_node
+
+    -- Add div snippet
+    luasnip.add_snippets("all", {
+      s("div", {
+        f(function()
+          -- Extract the comment prefix from commentstring (e.g. "// %s" -> "//")
+          local cs = vim.bo.commentstring
+          local prefix = cs:match("^(.-)%s*%%s") or "//"
+          local line = prefix .. " " .. string.rep("=", 96 - #prefix - 1)
+          return { line, prefix .. " " }
+        end),
+        i(1, "Replace me"),
+        f(function()
+          local cs = vim.bo.commentstring
+          local prefix = cs:match("^(.-)%s*%%s") or "//"
+          local line = prefix .. " " .. string.rep("=", 96 - #prefix - 1)
+          return { "", line }
+        end),
+      }),
+    })
   end
 
   local loader_ok, loader = pcall(require, "luasnip.loaders.from_vscode")
