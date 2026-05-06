7ac2a6b01aff293009d4d76aefd8ed384ca3932d
Jonas Cederberg <jonas.cederberg@gmail.com>
2026-05-06 11:27:28 +0200

    Remove markview
    Not needed anymore?

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -152,13 +152,6 @@ return {
     end,
   },
 
-  -- Markdown rendering (hover docs, README files)
-  {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-  },
-
   -- Syntax highlighting
   {
     "nvim-treesitter/nvim-treesitter",
@@ -211,9 +204,13 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      { "williamboman/mason.nvim", config = true },
+      { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
       "williamboman/mason-lspconfig.nvim",
-      { "j-hui/fidget.nvim", opts = {} },
+      -- Useful status updates for LSP.
+      -- This is the progress messages located at the bottom right
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { "j-hui/fidget.nvim",       opts = {} },
+      -- Allows extra capabilities provided by nvim-cmp
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
@@ -263,6 +260,9 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
@@ -310,6 +310,122 @@ return {
       })
     end,
   },
+
+  -- {
+  --   "folke/snacks.nvim",
+  --   priority = 1000,
+  --   lazy = false,
+  --   opts = {
+  --     -- your configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --     lazygit = { enabled = true },
+  --     notifier = { enabled = true },
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>n",
+  --       function()
+  --         Snacks.notifier.show_history()
+  --       end,
+  --       desc = "Notification History",
+  --     },
+  --     {
+  --       "<leader>bd",
+  --       function()
+  --         Snacks.bufdelete()
+  --       end,
+  --       desc = "Delete Buffer",
+  --     },
+  --     {
+  --       "<leader>gB",
+  --       function()
+  --         Snacks.gitbrowse()
+  --       end,
+  --       desc = "Git Browse",
+  --     },
+  --     {
+  --       "<leader>gb",
+  --       function()
+  --         Snacks.git.blame_line()
+  --       end,
+  --       desc = "Git Blame Line",
+  --     },
+  --     {
+  --       "<leader>gf",
+  --       function()
+  --         Snacks.lazygit.log_file()
+  --       end,
+  --       desc = "Lazygit Current File History",
+  --     },
+  --     {
+  --       "<leader>gg",
+  --       function()
+  --         Snacks.lazygit()
+  --       end,
+  --       desc = "Lazygit",
+  --     },
+  --     {
+  --       "<leader>gl",
+  --       function()
+  --         Snacks.lazygit.log()
+  --       end,
+  --       desc = "Lazygit Log (cwd)",
+  --     },
+  --     {
+  --       "<leader>un",
+  --       function()
+  --         Snacks.notifier.hide()
+  --       end,
+  --       desc = "Dismiss All Notifications",
+  --     },
+  --     {
+  --       "]]",
+  --       function()
+  --         Snacks.words.jump(vim.v.count1)
+  --       end,
+  --       desc = "Next Reference",
+  --       mode = { "n", "t" },
+  --     },
+  --     {
+  --       "[[",
+  --       function()
+  --         Snacks.words.jump(-vim.v.count1)
+  --       end,
+  --       desc = "Prev Reference",
+  --       mode = { "n", "t" },
+  --     },
+  --   },
+  --   init = function()
+  --     vim.api.nvim_create_autocmd("User", {
+  --       pattern = "VeryLazy",
+  --       callback = function()
+  --         -- Setup some globals for debugging (lazy-loaded)
+  --         _G.dd = function(...)
+  --           Snacks.debug.inspect(...)
+  --         end
+  --         _G.bt = function()
+  --           Snacks.debug.backtrace()
+  --         end
+  --         vim.print = _G.dd -- Override print to use snacks for `:=` command
+  --
+  --         -- Create some toggle mappings
+  --         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+  --         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+  --         Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+  --         Snacks.toggle.diagnostics():map("<leader>ud")
+  --         Snacks.toggle.line_number():map("<leader>ul")
+  --         Snacks.toggle
+  --             .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+  --             :map("<leader>uc")
+  --         Snacks.toggle.treesitter():map("<leader>uT")
+  --         Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+  --         Snacks.toggle.inlay_hints():map("<leader>uh")
+  --       end,
+  --     })
+  --   end,
+  -- },
+
 
   -- Code outline
   {
