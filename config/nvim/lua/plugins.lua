45cff3173479e7fd4c389144659402405f7d4739
Jonas Cederberg <jonas.cederberg@gmail.com>
2026-05-06 11:11:37 +0200

    Add obsidian

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -241,6 +241,47 @@ return {
       require("configs.mason").config()
     end,
   },
+  -- Obsidian plugin
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "hjarnan",
+          path = "~/Dropbox/Apps/Obsidian/Dharma",
+        },
+      --   {
+      --     name = "work",
+      --     path = "~/vaults/work",
+      --   },
+      },
+      -- Remove standard name/ID on creation that adds zettlekasten format
+      note_id_func = function(title)
+        return title
+      end,
+      follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        vim.fn.jobstart({"xdg-open", url})  -- linux
+      end,
+      -- see below for full list of options 👇
+    },
+  },
 
   -- Autoformat
   {
